12f1ed6f821b633e521c043663c6e8890b1284e0
coolgray06 <6636@holbertonstudents.com>
2023-08-24 12:11:17 -0700

    bubble sort

--- sort.h
+++ sort.h
@@ -4,6 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values
+ * @false: Equals 0
+ * true: Equals 1
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
