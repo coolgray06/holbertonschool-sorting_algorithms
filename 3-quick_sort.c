ba435146e863fdbc387df780dc35df9418ae8936
coolgray06 <6636@holbertonstudents.com>
2023-08-24 20:47:02 -0700

    quick

---
+++ 3-quick_sort.c
@@ -0,0 +1,94 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: First integer to swap.
+ * @b: Second integer.
+ */
+
+void swap_ints(int *a, int *b)
+{
+        int tmp;
+
+        tmp = *a;
+        *a = *b;
+        *b = tmp;
+}
+
+/**
+ * lomuto_partition - Selected sorting scheme.
+ * @array: Array of integers.
+ * @size: Size of array.
+ * @left: Starting index
+ * @right: Ending index
+ * Return: Final partition index.
+ */
+
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *driver, up, down;
+
+	driver = array + right;
+	for (up = down = left; down < right; down++)
+	{
+		if (array[down] < *driver)
+		{
+			if (up < down)
+			{
+				swap_ints(array + down, array + up);
+				print_array(array, size);
+			}
+			up++;
+		}
+	}
+
+	if (array[up] > *driver)
+	{
+		swap_ints(array + up, driver);
+		print_array(array, size);
+	}
+
+	return (up);
+}
+
+/**
+ * lomuto_sort - Implementing quicksort through recursion.
+ * @array: Array of integers.
+ * @size: Size of array.
+ * @left: Starting index
+ * @right: Ending index
+ * Return: Final partition index.
+ */
+
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int belong;
+	
+	if (right - left > 0)
+	{
+		belong = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, belong - 1);
+		lomuto_sort(array, size, belong + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of ints in ascending order
+ * using quicksort.
+ * @array: An array of integers.
+ * @size: Size of array
+ * Return: Print array after swap
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
